7876c4d80c770704d1bbfa3eaec3b8be4cff39db
C.Leela Vardhan Reddy <cleelavardhanreddy@gmail.com>
2026-07-20 12:19:39 +0530

    Delete SPI_defines.h

--- SPI_defines.h
+++
@@ -1,8 +0,0 @@
-#include <LPC21xx.h>
-
-#define PULL_CS IOCLR0 = (1<<7);
-#define PUSH_CS IOSET0 = (1<<7);
-#define READ_MODE         0x03
-#define WRITE_MODE        0x02
-#define WRITE_ENABLE      0x06
-#define WRITE_DISABLE     0x04
